0ab5331e047517570e23af708f9b61e9beabddf8
mts7 <github@mts7.com>
2021-09-29 15:03:26 -0400

    added Vue with Vue Router and TypeScript

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header">Example Component</div>
+
+					<div class="card-body">
+						I'm an example component with a custom phrase.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class ExampleComponent extends Vue {
+	mounted() {
+		console.log('Component mounted.');
+	}
+}
+</script>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h1>Header Goes Here</h1>
+
+		<div class="container">
+			<router-view></router-view>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'App',
+}
+</script>
